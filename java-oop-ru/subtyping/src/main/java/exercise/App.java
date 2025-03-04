dc554264e8e1574686966b996800ac17da035a0a
@hexlet/cli <support@hexlet.io>
2025-03-04 23:42:14 +0200

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,19 +1,26 @@
 package exercise;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 // BEGIN
 public class App {
 
     public static void swapKeyValue(KeyValueStorage storage) {
         var storageMap = new HashMap<>(storage.toMap());
-        var swipeMap = new HashMap<String, String>();
+        var swappedMap = new HashMap<String, List<String>>();
 
-        storageMap.forEach((key, value) -> swipeMap.put(value, key));
+        storageMap.forEach((key, value) -> {
+            swappedMap.computeIfAbsent(value, k -> new ArrayList<>()).add(key);
+            storage.unset(key);
+        });
 
-        for (var entry : swipeMap.entrySet()) {
-            storage.set(entry.getKey(), entry.getValue());
-        }
+        swappedMap.forEach((value, keys) -> {
+            for (var key : keys) {
+                storage.set(value, key);
+            }
+        });
     }
 }
 
