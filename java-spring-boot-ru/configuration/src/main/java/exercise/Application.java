43e578902a477221838516f87cc31b3897569ca6
@hexlet/cli <support@hexlet.io>
2025-07-30 22:28:44 +0200

    submit java-spring-boot-ru/configuration

--- java-spring-boot-ru/configuration/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/configuration/src/main/java/exercise/Application.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import exercise.model.User;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 @RestController
