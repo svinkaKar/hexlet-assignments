3a6e33c8a8e1cddc045f9880f91c20fbe8819d92
@hexlet/cli <support@hexlet.io>
2025-07-30 22:27:27 +0200

    submit java-spring-boot-ru/configuration

--- java-spring-boot-ru/configuration/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/configuration/src/main/java/exercise/Application.java
@@ -2,16 +2,12 @@ package exercise;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import  org.springframework.beans.factory.annotation.Autowired;
-
 import exercise.model.User;
-import exercise.component.UserProperties;
 
 @SpringBootApplication
 @RestController
@@ -38,7 +34,6 @@ public class Application {
                 .sorted()
                 .collect(Collectors.toList());
     }
-    
     // END
 
     @GetMapping("/users")
