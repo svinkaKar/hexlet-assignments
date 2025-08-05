53320714c695fcf792d399ab2e6b30c7d9fcb013
@hexlet/cli <support@hexlet.io>
2025-08-05 21:45:06 +0200

    submit java-spring-boot-ru/jpa-auditing

--- java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
+++ java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.EntityListeners;
-import static jakarta.persistence.GenerationType.IDENTITY;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
